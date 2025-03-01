868dc99a94e538e16412c6cc27d3eee2f44bf4ec
jaime <jaime@192.168.1.132>
2025-03-01 23:03:30 +0100

    Version definitiva

--- src/cuentas/clientes/Main.java
+++ src/cuentas/clientes/Main.java
@@ -1,27 +1,50 @@
-//Clase Main, comentario para realizar una nueva version
-
 package cuentas.clientes;
 
+/**
+ * Clase Principal para la ejecución del programa de cuenta bancaria
+ * @author Jaime González Fernández
+ */
 public class Main {
 
+    /**
+     * Método Principal
+     * @param LLama al método operativa_cuenta 
+     */
     public static void main(String[] args) {
             operativa_cuenta(5);
     }
 
+    /**
+     * Método para la operativa de la cuenta
+     * @param Devuelve la cantidad 
+     */
     private static void operativa_cuenta(float cantidad) {
         Cuenta cuenta1;
         double saldoActual;
 
+        /**
+         * Crea un objeto de tipo cuenta con atributos ya asignados
+         */
         cuenta1 = new Cuenta("Rafael Nadal Parera","1000-2365-85-1230456789",2500,0);
+        
+        /**
+         * Imprime el saldo actual
+         */
         System.out.println("El saldo actual es:"+ cuenta1.estado() );
 
         try {
+            /**
+             * Intento de retirada de dinero
+             */
             cuenta1.retirar(2300);
             System.out.println("El saldo actual es:"+ cuenta1.estado() );
         } catch (Exception e) {
             System.out.print("Fallo al retirar");
         }
         try {
+            /**
+             * Intento de ingreso de dinero
+             */
             System.out.println("Ingreso en cuenta");
             cuenta1.ingresar(695);
             System.out.println("El saldo actual es:"+ cuenta1.estado() );
